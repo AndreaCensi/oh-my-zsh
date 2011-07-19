6bf9d6d575ad380017f83502b67ccc188bc4ab26
Andrea Censi <andrea@cds.caltech.edu>
2011-07-18 20:32:43 -0700

    experiments with title

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -3,9 +3,14 @@
 #Fully support screen, iterm, and probably most modern xterm and rxvt
 #Limited support for Apple Terminal (Terminal can't set window or tab separately)
 function title {
-  if [[ "$TERM" == "screen" ]]; then 
+  #echo "My term: $TERM ; title: $1"
+  if [[ "$TERM" == screen* ]]; then 
     print -Pn "\ek$1\e\\" #set screen hardstatus, usually truncated at 20 chars
-  elif [[ ($TERM == "xterm") ]] || [[ ($TERM == "rxvt") ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
+    
+    print -Pn "\e]0;M0\a"
+    print -Pn "\e]1;M1\a"
+    print -Pn "\e]2;M2\a"
+  elif [[ ($TERM == xterm*) ]] || [[ ($TERM == "xterm-color") ]] || [[ ($TERM == "rxvt") ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
     print -Pn "\e]2;$2\a" #set window name
     print -Pn "\e]1;$1\a" #set icon (=tab) name (will override window name on broken terminal)
   fi
@@ -16,17 +21,34 @@ function title {
 
 ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
 ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
+ # local SHORT_PWD="%8<..<%~%<<" #15 char left truncated PWD
 
-#Appears when you have the prompt
 function precmd {
-#  title $ZSH_THEME_TERM_TAB_TITLE_IDLE $ZSH_THEME_TERM_TITLE_IDLE
-    title "$MY_TERM_TITLE" "$MY_TERM_TITLE"
+  local SHORT_PWD=`basename $PWD`
+  
+  if [[ "$TERM" == screen* ]]; then 
+    title "$TERM_TOPIC$SHORT_PWD"
+  elif [[ ($TERM == xterm*) ]]; then
+    title "$MY_TERM_TITLE:$SHORT_PWD" "$MY_TERM_TITLE:$SHORT_PWD"
+  fi
+    
 }
 
-#Appears at the beginning of (and during) of command execution
-function preexec {
-  local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]} #cmd name only, or if this is sudo or ssh, the next cmd
-#  title "$CMD" "%100>...>$2%<<"
+function topic {
+    export TERM_TOPIC="$1|"
+}
 
-    title "$MY_TERM_TITLE" "$MY_TERM_TITLE"
+function preexec {
+    emulate -L zsh
+    setopt extended_glob
+    local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]} #cmd name only, or if this is sudo or ssh, the next cmd
+
+    local SHORT_PWD=`basename $PWD`
+
+    if [[ "$TERM" == screen* ]]; then 
+        title "$TERM_TOPIC$SHORT_PWD:$CMD" "$TERM_TOPIC$SHORT_PWD:$CMD"
+    elif [[ ($TERM == xterm*) ]]; then
+        title "$MY_TERM_TITLE:$SHORT_PWD:$CMD" "$MY_TERM_TITLE:$SHORT_PWD:$CMD"
+    fi
+
 }
